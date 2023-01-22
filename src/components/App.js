c1dc641545466b2becf2de2114c42601920a418e
Loise <loise.kimani@student.moringaschool.com>
2023-01-22 12:40:45 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-  const [isDark,setDark] = useState(false)
-  
-  function newMode() {
-    setDark(!isDark)
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function handleDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
   }
-
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-
-
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isDarkMode ? "App dark" : "App light";
 
   return (
-    <div className="App Dark">
+    <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button onClick={newMode} className={isDark ? "App Dark" :"App"} >Dark Mode</button>
+        <button onClick={handleDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
@@ -27,3 +25,33 @@ function App() {
 }
 
 export default App;
+
+
+// import React, {useState} from "react";
+// import ShoppingList from "./ShoppingList";
+// import itemData from "../data/items";
+
+// function App() {
+//   const [isDark,setDark] = useState(false)
+//   function newMode() {
+//     setDark(!isDark)
+//   }
+
+//   // replace 'false' with a state variable that can be toggled between true and false
+//   // this will be used for the Dark Mode Toggle feature
+
+
+//   const appClass = false ? "App dark" : "App light"
+
+//   return (
+//     <div className={appClass}>
+//       <header>
+//         <h2>Shopster</h2>
+//         <button onClick={newMode} >Dark Mode</button>
+//       </header>
+//       <ShoppingList items={itemData} />
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import Item from "./Item";
 
+
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleChange(e) {
+    setSelectedCategory(e.target.value);
+  }
+  let  filteredItems= items.filter((item) => {
+    if (selectedCategory === "All") return true;
+    return item.category === selectedCategory;
+  });
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -22,3 +32,28 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+// import React from "react";
+// import Item from "./Item";
+
+// function ShoppingList({ items }) {
+//   return (
+//     <div className="ShoppingList">
+//       <div className="Filter">
+//         <select name="filter">
+//           <option value="All">Filter by category</option>
+//           <option value="Produce">Produce</option>
+//           <option value="Dairy">Dairy</option>
+//           <option value="Dessert">Dessert</option>
+//         </select>
+//       </div>
+//       <ul className="Items">
+//         {items.map((item) => (
+//           <Item key={item.id} name={item.name} category={item.category} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ShoppingList;
